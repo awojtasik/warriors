4e9baf9c6707d9e8e22880d6e31249805d615566
AgaWojtasik <agawojtasik25@wp.pl>
2022-02-09 19:31:32 +0100

    continuation of the preparation of the environment

--- index.ts
+++ index.ts
@@ -1,2 +1,32 @@
-// method-override => za pomocę formularzy moemy korzystać z metod http
-console.log('Hello world!');
+import * as express from 'express';
+import 'express-async-errors';
+import * as methodOverride from 'method-override';
+import { static as eStatic, urlencoded } from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(eStatic('public'));
+app.engine(
+  '.hbs',
+  engine({
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// app.use(handleErrors)
+
+app.listen(3000, 'localhost', () => {
+  console.log('listening on http://localhost:3000');
+});
